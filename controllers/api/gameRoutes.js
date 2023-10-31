eeea9e1bab67eaf6de3679683f8e28a2af8d9f40
Corey <coreyvladesov2023@u.northwestern.edu>
2023-10-31 14:15:44 -0500

    added in seed data and edited routes

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,1 +1,55 @@
+const router = require('express').Router();
+const { Games, Users } = require('../models');
+const withAuth = require('../utils/auth');
+
+// root address
+router.get('/', async (req, res) => {
+    try {
+        // Get all projects and JOIN with user data
+        const gameData = await Games.findAll({
+            include: [
+                {
+                    model: Users,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        
+        // Serialize data so the template can read it
+        const games = gameData.map((game) => game.get({ plain: true }));
+        
+        // Pass serialized data and session flag into template
+        res.render('Games', { 
+            games, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// root then find by specific ID
+router.get('/games/:id', async (req, res) => {
+    try {
+        const gameData = await Games.findByPk(req.params.id, {
+            include: [
+        {
+            model: Users,
+            attributes: ['name'],
+        },
+    ],
+});
+
+const game = gameData.get({ plain: true });
+
+res.render('games', {
+    ...game,
+    logged_in: req.session.logged_in
+});
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
+
 // root/api/games
