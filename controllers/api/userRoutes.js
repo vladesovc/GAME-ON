9f8eca7ea6e66c3aa6e8c2779cfa92ff2608af6a
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-31 12:00:51 -0500

    added signup/login static js, linked to login handlebar, added basic route structure to controllers files

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// root/api/users
+
+router.post('/signup', async (req, res) => {
+
+});
+
+router.post('/login', async (req, res) => {
+
+});
+
+router.post('/logout', async (req, res) => {
+
+});
