b461e493c7fece381c5854b0128a799844a04b2f
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-11-01 10:57:44 -0500

    cleaned up seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,27 +1,3 @@
-// const sequelize = require('../config/connection.js');
-// const { Users, Games } = require('../models/index.js');
-
-// const userData = require('./userData.json');
-// const gameData = require('./gameData.json');
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   await Users.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-
-//   await Games.bulkCreate(gameData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-
-//   process.exit(0);
-// };
-
-// seedDatabase();
-
 const sequelize = require('../config/connection');
 const seedUsers = require('./UsersData');
 const seedGames = require('./GamesData.js');
