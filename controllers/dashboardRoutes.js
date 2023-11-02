b4f5c82e8c22f86faa7fc83eb2eb3133e365bcdb
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-11-02 15:03:44 -0500

    working out dashboard front end.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -7,7 +7,7 @@ router.get('/', withAuth, async (req, res) => {
     try {
         const userData = await Users.findByPk(req.session.user_id, {
             attributes: { exclude: ['password', 'email'] },
-            include: [{ model: Games, through: { model: Reviews }, as: 'user_reviews' }],
+            include: [{ model: Games, through: { model: Saved }, as: 'saved_games' }],
         });
 
         const user = userData.get({ plain: true });
@@ -22,24 +22,24 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 
-router.get('/profile', withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Project }],
-      });
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(req.session.user_id, {
+//         attributes: { exclude: ['password'] },
+//         include: [{ model: Project }],
+//       });
   
-      const user = userData.get({ plain: true });
+//       const user = userData.get({ plain: true });
   
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+//       res.render('profile', {
+//         ...user,
+//         logged_in: true
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 
 module.exports = router;
